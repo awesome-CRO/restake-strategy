75ccb00a5f8eb6bf2257d2b14ef120ec0823ca53
Tianhao Zhou <tianhaoz@umich.edu>
2021-12-17 01:18:22 -0800

    feat: add analyzer apis

--- src/reward.py
+++ src/reward.py
@@ -5,19 +5,30 @@ from src.config import StakingConfigSingleton
 
 class RewardAnalyzer:
     def __init__(self) -> None:
-        self.current_amount = StakingConfigSingleton.get().init_amount
+        self.init_amount = StakingConfigSingleton.get().init_amount
         self.rate = StakingConfigSingleton.get().staking_reward_rate
         self.cost = StakingConfigSingleton.get().restake_cost
         self.annual_length = 365.0
 
     def compute_possible_rewards(self,
                                  interval_range: List[float]) -> List[float]:
-        raise NotImplementedError()
+        total_amount_list = []
+        for interval_length in interval_range:
+            total_amount = self.compute_total_amount_with_interval_length(
+                interval_length)
+            total_amount_list.append(total_amount)
+        return total_amount_list
 
-    def _compute_total_reward_with_interval(self, interval: float) -> float:
-        return 0
+    def compute_total_amount_with_interval_length(
+            self, interval_length: float) -> float:
+        cycles = int(self.annual_length / interval_length)
+        current_amount = self.init_amount
+        for _ in range(cycles):
+            interval_reward = self.compute_interval_reward(interval_length)
+            current_amount += (interval_reward - self.cost)
+        return current_amount
 
-    def _compute_interval_reward(self, interval_length: float) -> float:
+    def compute_interval_reward(self, interval_length: float) -> float:
         annual_reward = self.current_amount * self.rate
         interval_reward = annual_reward * interval_length / self.annual_length
         return interval_reward
